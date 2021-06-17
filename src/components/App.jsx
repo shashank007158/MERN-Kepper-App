2ae9cd1a3ce164937b936a1bcd5502d38bfbc87d
shashank <shashankjyrt@gmail.com>
2021-06-17 14:43:38 +0530

    Style Change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,19 @@ import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateNote";
 import axios from "axios";
+import {Container,Row,Col} from "react-bootstrap";
 
 function App() {
   const [notes, setNotes] = useState([]);
-  axios.get("https://mern-notes-keeper.herokuapp.com").then(res=>setNotes(res.data));
+  axios.get("http://mern-notes-keeper.herokuapp.com").then(res=>setNotes(res.data));
   
   function addNote(newNote) {
     const notes = {
       title: newNote.title,
       content: newNote.content,
     };
-    axios.post("https://mern-notes-keeper.herokuapp.com/add", notes).then(
-      axios.get("https://mern-notes-keeper.herokuapp.com").then((res) => {
+    axios.post("http://mern-notes-keeper.herokuapp.com/add", notes).then(
+      axios.get("http://mern-notes-keeper.herokuapp.com").then((res) => {
         res.data.length > 0 && setNotes(res.data);
       })
     );
@@ -23,13 +24,19 @@ function App() {
 
   function deleteNote(id) {
     axios
-      .delete("https://mern-notes-keeper.herokuapp.com/" + id);
+      .delete("http://mern-notes-keeper.herokuapp.com/d" + id);
   }
 
   return (
     <div>
       <Header />
+      <Container fluid="md">
+      <Row>
+      <Col>
       <CreateArea onAdd={addNote} />
+      </Col>
+      </Row>
+      </Container>
       {notes.map((noteItem, index) => {
         return (
           <Note
@@ -43,6 +50,7 @@ function App() {
       })}
       <Footer />
     </div>
+    
   );
 }
 
